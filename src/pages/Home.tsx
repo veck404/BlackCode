16d91b6d2113e299893e30d9f8ab532ff2748f6d
veck404 <victor7ultimate@gmail.com>
2025-09-23 21:58:08 +0100

    fh

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="overflow-hidden">
-      <HeroSection 
+      <HeroSection />
       <StatsSection />
       <FeaturesSection />
 
